e04ea23f08eedbec3ba35147cd63bd6fb328bfdf
murzin66 <126623679+murzin66@users.noreply.github.com>
2023-12-24 22:51:35 +0300

    add lab3 part 2/2

--- store/urls.py
+++ store/urls.py
@@ -2,10 +2,14 @@
 
 from django.urls import path
 from .views import products_view, shop_view, products_page_view
+from .views import cart_view, cart_add_view, cart_del_view
 
 urlpatterns = [
     path('product/', products_view),
     path('product/<slug:page>.html',products_page_view),
     path('product/<int:page>', products_page_view),
     path('', shop_view),
+    path('cart/', cart_view),
+    path('cart/add/<str:id_product>', cart_add_view),
+    path('cart/del/<str:id_product>', cart_del_view),
 ]
--- store/views.py
+++ store/views.py
@@ -1,20 +1,33 @@
 from django.http import JsonResponse
 from .models import DATABASE
 from django.http import HttpResponse,HttpResponseNotFound
+from logic.services import filtering_category
+from logic.services import view_in_cart, add_to_cart, remove_from_cart
+
+
 
 def products_view(request):
     if request.method == "GET":
-        id = request.GET.get('id')
-        if id :
-            if id in DATABASE:
-                return JsonResponse(DATABASE[id], json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})
-            else:
-                return HttpResponseNotFound('Данного продукта нет в базе данных')
-        else:
-            return  JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})
+        # Обработка id из параметров запроса (уже было реализовано ранее)
+        if id_product := request.GET.get("id"):
+            if data := DATABASE.get(id_product):
+                return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                             'indent': 4})
+            return HttpResponseNotFound("Данного продукта нет в базе данных")
 
+        # Обработка фильтрации из параметров запроса
+        category_key = request.GET.get("category")  # Считали 'category'
+        ordering_key = request.GET.get("ordering")
+        if ordering_key: # Если в параметрах есть 'ordering'
+            if request.GET.get("reverse") in ('true', 'True'): # Если в параметрах есть 'ordering' и 'reverse'=True
+                data = data = filtering_category(DATABASE, category_key, ordering_key, True)
+            else:
+                data = data = filtering_category(DATABASE, category_key,ordering_key)
+        else:
+            data = data = filtering_category(DATABASE, category_key)
+        # В этот раз добавляем параметр safe=False, для корректного отображения списка в JSON
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False,
+                                                                 'indent': 4})
 
 def shop_view(request):
     if request.method == "GET":
@@ -43,4 +56,33 @@ def products_page_view(request, page):
         return HttpResponse(status=404)
 
 
+def cart_view(request):
+    if request.method == "GET":
+        data = view_in_cart() # TODO Вызвать ответственную за это действие функцию
+        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                     'indent': 4})
+
+
+def cart_add_view(request, id_product):
+    if request.method == "GET":
+        result = add_to_cart(id_product) # TODO Вызвать ответственную за это действие функцию
+        if result:
+            return JsonResponse({"answer": "Продукт успешно добавлен в корзину"},
+                                json_dumps_params={'ensure_ascii': False})
+
+        return JsonResponse({"answer": "Неудачное добавление в корзину"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})
+
+
+def cart_del_view(request, id_product):
+    if request.method == "GET":
+        result = remove_from_cart(id_product) # TODO Вызвать ответственную за это действие функцию
+        if result:
+            return JsonResponse({"answer": "Продукт успешно удалён из корзины"},
+                                json_dumps_params={'ensure_ascii': False})
+
+        return JsonResponse({"answer": "Неудачное удаление из корзины"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})
 
