bebe227584ae5e1f03bbef6a841d558ee029fdac
murzin66 <126623679+murzin66@users.noreply.github.com>
2024-01-28 15:02:20 +0300

    add lab 2

--- Course Python русский/Атрибуты и методы/Лабораторная 2/Класс Book/main.py
+++ Course Python русский/Атрибуты и методы/Лабораторная 2/Класс Book/main.py
@@ -13,8 +13,26 @@ BOOKS_DATABASE = [
 
 
 # TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        if not isinstance(id_, int):
+            raise TypeError("ID должно быть целым числом")
+        if id_ <= 0:
+            raise ValueError("ID должно быть положительным числом")
+        self.id_ = id_
+        if not isinstance(name, str):
+            raise TypeError("name должно быть строкой")
+        self.name = name
+        if not isinstance(pages, int):
+            raise TypeError("pages должно быть целым числом")
+        if pages <= 0:
+            raise ValueError("pages должно быть положительным числом")
+        self.pages = pages
+    def __repr__(self):
+        return f'{self.__class__.__name__}(id_={self.id_}, name=\'{self.name}\', pages={self.pages})'
 
-
+    def __str__(self):
+        return f'Книга "{self.name}"'
 if __name__ == '__main__':
     # инициализируем список книг
     list_books = [
--- Course Python русский/Атрибуты и методы/Лабораторная 2/Класс Library/main.py
+++ Course Python русский/Атрибуты и методы/Лабораторная 2/Класс Library/main.py
@@ -13,9 +13,48 @@ BOOKS_DATABASE = [
 
 
 # TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        if not isinstance(id_, int):
+            raise TypeError("ID должно быть целым числом")
+        if id_ <= 0:
+            raise ValueError("ID должно быть положительным числом")
+        self.id_ = id_
+        if not isinstance(name, str):
+            raise TypeError("name должно быть строкой")
+        self.name = name
+        if not isinstance(pages, int):
+            raise TypeError("pages должно быть целым числом")
+        if pages <= 0:
+            raise ValueError("pages должно быть положительным числом")
+        self.pages = pages
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(id_={self.id_}, name={self.name}, pages={self.pages})'
+
+    def __str__(self):
+        return f'Книга: {self.name!r}'
 
 
 # TODO написать класс Library
+class Library:
+    def __init__(self, books: list=None):
+        self.books = books
+
+    def get_next_book_id(self) -> int:
+        if not self.books:
+            return 1
+        max_ = 0
+        for book in self.books:
+            if max_<book.id_:
+                max_ = book.id_
+        return max_+1
+
+    def get_index_by_book_id(self,id) -> int:
+        for i, book in enumerate (self.books):
+            if book.id_ == id:
+                return i
+        raise ValueError("Книги с запрашиваемым id не существует")
 
 
 if __name__ == '__main__':
